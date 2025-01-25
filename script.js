2ef84126a3a507266bc6e85a276d1f42e0f2f1a7
Synamin <125604267+Synamin@users.noreply.github.com>
2025-01-24 23:30:41 -0500

    First Assignment
    Code for first assignment

---
+++ script.js
@@ -0,0 +1,43 @@
+0; //this is just a 0, also this is java script btw
+3858356;
+28476.57356;
+Infinity; //this is a number
+
+
+var kitty; //declared a variable for store space in memory
+
+kitty = 5; //definition with a data type of number with the value 5
+
+var Kitty = 24; //semi colon are end of statement, this line has declaration and definition
+
+console.log(kitty); //will print whatever is in the parentheses
+
+kitty = Kitty; // = assignment operator, assign left to right
+
+console.log(kitty);
+
+var doggy; //declared a variable but not defined a value
+console.log(doggy); //undefined (means nothing)
+
+//numbers, undefined, strings
+var prose = 'uurfhuer ufgyur wuf gfruywfbuyw    '; // ' is a string  (deliminitor) declaration
+var moreprose = "i said 'something' bhebcherhb   "; //
+
+//booleans are either true or false
+
+var x = true;
+x = false;
+
+// objects
+var ball = {}; //object is defined by curly brackets; empty object
+ball = null; //null is a data type in js; undefined but only objects
+
+//operators
+5 + 5;
+kitty = 5 + 5;
+var kitty; //becomes undefined because we are noe referring to a new place in memory
+
+kitty = 5 + "5"; //string concatenation operator
+console.log(kitty);
+
+console.log(kitty + "fuhfuewbyew");
---
+++ sketch.js
@@ -0,0 +1,32 @@
+var x = 0;
+var y = 0;
+var a = 0;
+var b = 0;
+
+function setup(){
+    createCanvas(720,480);
+}
+
+function draw(){
+    background('#AA44FF') //automatic semicolon insertion
+    fill("pink");
+    rect(x,y,10,10);
+    x = x + 1;
+    x = x % 500; //x assignment operator x; modulo operator (divided operator)
+    y = y + 2;
+    y = y % 600;
+    
+    circle(a,b,20,20)
+    a = a + 1;
+    a = a % 400;
+    b = b + 5;
+    b = b % 600;
+
+    fill("blue")
+    rect(0,300,600,200);
+
+    textSize(75)
+    text("🏄🏽‍♀️", 0 + y, 300) //flower
+    text("🚣🏽‍♀️", 100 + x, 300) //ladybug
+
+}
