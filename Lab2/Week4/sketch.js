9c95d233b602bf6cef65d57be82f9c338e680ae4
Synamin <125604267+Synamin@users.noreply.github.com>
2025-02-08 03:36:39 -0500

    whoops

--- Lab2/Week4/sketch.js
+++ Lab2/Week4/sketch.js
@@ -1,24 +1,49 @@
-var redBrick = {
-    x: 0,
-    y: 0,
+var pinkTriangle = {
+    x: 40,
+    y: 44,
+    a: 20,
+    b: 60,
+    d: 50,
+    e: 90,
     w: 30,
-    h: 30,
+    h: 5,
     xSpeed: 1,
     ySpeed: 1,
-    colour: 'red',
+    aSpeed: 1,
+    bSpeed: 1,
+    dSpeed: 1,
+    eSpeed: 1,
+
+    colour: 'magenta',
     draw: function(){
         fill( this.colour );
-        rect(this.x, this.y, this.w, this.h);
+        triangle(this.x, this.y, this.a, this.b, this.d, this.e);
     },
     move: function(){
         this.x += this.xSpeed;
         this.y += this.ySpeed;
+        this.a += this.aSpeed;
+        this.b += this.bSpeed;
+        this.d += this.dSpeed;
+        this.e += this.eSpeed;
 
         if(this.x < 0 || this.x > width - this.w){
             this.xSpeed *= -1;
         }
         if(this.y > height - this.h || this.y < 0){
             this.ySpeed *= -1;
+        }
+        if(this.a < 0 || this.a > width - this.w){
+            this.aSpeed *= -1;
+        }
+        if(this.b > height - this.h || this.b < 0){
+            this.bSpeed *= -1;
+        }
+        if(this.d < 0 || this.d > width - this.w){
+            this.dSpeed *= -1;
+        }
+        if(this.e > height - this.h || this.e < 0){
+            this.eSpeed *= -1;
         }
     }
 };
@@ -56,8 +81,8 @@ function setup(){
 
 function draw(){
     background('grey'); 
-    redBrick.draw();
-    redBrick.move();
+    pinkTriangle.draw();
+    pinkTriangle.move();
     blueBrick.draw();
     blueBrick.move();
 }
