e6a7354badebe6b1b4518b3e974af68ed316fee0
Synamin <125604267+Synamin@users.noreply.github.com>
2025-02-15 03:04:51 -0500

    lab3

---
+++ lab3/Week5/sketch.js
@@ -0,0 +1,58 @@
+var colourPicker; // function scope
+let strokeWeightSlider;
+var bgColourPicker;
+let input;
+
+function setup(){
+    createCanvas(720,300);
+
+    colourPicker = createColorPicker('pink');
+    
+    strokeWeightSlider = createSlider(1,10,5,1);
+    
+    bgColourPicker = createColorPicker('gray'); 
+    
+    var bgColorButton = createButton('Refresh');
+    bgColorButton.mousePressed(repaint);
+    bgColourPicker.changed( repaint ); 
+    background( bgColourPicker.value() );
+
+    // remixed from https://p5js.org/reference/p5/createInput/
+    input = createInput();
+    input.position(8,360);
+    /// end remix
+
+    var surprise = createButton('CLICK ME')
+    surprise.mouseClicked(boom);
+    surprise.changed(boom);
+    background(surprise.value());
+}
+
+function draw(){
+    // ellipse(mouseX, mouseY, 10,10);
+    strokeWeight( strokeWeightSlider.value() );
+    stroke( colourPicker.value() );   
+
+    // remixed from p5js.org/reference/mouseispressed/
+    if(mouseIsPressed){
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+    /// end remix
+
+    // remixed from https://p5js.org/reference/p5/createInput/
+    let textBox = input.value();
+    text(textBox, 30,30);
+    /// end remix
+}
+
+function repaint(){
+    background( bgColourPicker.value() );
+    console.log( bgColourPicker.value().setGreen(255) );
+}
+
+function boom(){
+    background('red');
+    textSize(150);
+    text("🔥", 100, 250);
+    text("💥", 300, 200);
+}
